dc8ac5e994b812a70b7884323161eb8e0164c8dc
Ethan Hogge <97705628+shuvll@users.noreply.github.com>
2026-08-12 15:45:00 -0600

    Button fix using a tag instead of button

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@
     <h1 class="hero__title">Ethan Hogge</h1>
     <p class="hero__subtitle">Software Developer. Full stack dev, defense systems to Vue 3 modernization</p>
     <img src="/src/assets/ethan-photo.jpg" alt="Ethan image" />
-    <button class="hero__button" @click="window.location.href='mailto:ethan@example.com'">Email Me</button>
-    <button class="hero__button" @click="window.location.href='https://www.linkedin.com/in/ethan-hogge/'">LinkedIn</button>
-    <button class="hero__button" @click="window.location.href='https://github.com/ethanhogge'">GitHub</button>
-    <button class="hero__button" @click="window.location.href='https://ethanhogge.com/resume.pdf'">Resume</button>
-    <button class="hero__button" @click="window.location.href=''">View my work</button>
+    <a class="hero__button" href='mailto:ethan@example.com'">Email Me</a>
+    <a class="hero__button" href='https://www.linkedin.com/in/ethan-hogge/'">LinkedIn</a>
+    <a class="hero__button" href='https://github.com/ethanhogge'">GitHub</a>
+    <a class="hero__button" href='https://ethanhogge.com/resume.pdf'">Resume</a>
+    <a class="hero__button" href=''">View my work</a>
   </div>
 </template>
