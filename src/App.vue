dc040315d519fde177e66c1921062d7fb8f8a5a3
Ethan Hogge <97705628+shuvll@users.noreply.github.com>
2026-08-12 15:41:58 -0600

    Button setup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
-  <img src="/src/assets/ethan-photo.jpg" alt="Ethan image" />
+  <div class="hero">
+    <h1 class="hero__title">Ethan Hogge</h1>
+    <p class="hero__subtitle">Software Developer. Full stack dev, defense systems to Vue 3 modernization</p>
+    <img src="/src/assets/ethan-photo.jpg" alt="Ethan image" />
+    <button class="hero__button" @click="window.location.href='mailto:ethan@example.com'">Email Me</button>
+    <button class="hero__button" @click="window.location.href='https://www.linkedin.com/in/ethan-hogge/'">LinkedIn</button>
+    <button class="hero__button" @click="window.location.href='https://github.com/ethanhogge'">GitHub</button>
+    <button class="hero__button" @click="window.location.href='https://ethanhogge.com/resume.pdf'">Resume</button>
+    <button class="hero__button" @click="window.location.href=''">View my work</button>
+  </div>
 </template>
