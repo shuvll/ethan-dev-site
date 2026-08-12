f1d18faaa3aa527aeb835b07a914d6a4e8b3e3ae
Ethan Hogge <97705628+shuvll@users.noreply.github.com>
2026-08-12 16:11:48 -0600

    Favicon changes

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@
     <h1 class="hero__title">Ethan Hogge</h1>
     <p class="hero__subtitle">Software Developer. Full stack dev, defense systems to Vue 3 modernization</p>
     <img src="/src/assets/ethan-photo.jpg" alt="Ethan image" />
-    <a class="hero__button" href='mailto:ethan@example.com'">Email Me</a>
-    <a class="hero__button" href='https://www.linkedin.com/in/ethan-hogge/'">LinkedIn</a>
-    <a class="hero__button" href='https://github.com/ethanhogge'">GitHub</a>
-    <a class="hero__button" href='https://ethanhogge.com/resume.pdf'">Resume</a>
-    <a class="hero__button" href=''">View my work</a>
+    <a class="hero__button" href='mailto:ethanhogge952@gmail.com'>Email Me</a>
+    <a class="hero__button" target="_blank" href='https://www.linkedin.com/in/ethan-hogge-a58070138/'>LinkedIn</a>
+    <a class="hero__button" target="_blank" href='https://github.com/shuvll'>GitHub</a>
+    <a class="hero__button" target="_blank" href='/public/ethan-hogge-resume.pdf'>Resume</a>
+    <a class="hero__button" href=''>View my work</a>
   </div>
 </template>
